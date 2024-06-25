adbd9021107156c26b308b3bbc1b75ab6440a475
Filipe <filipelehmannp@gmail.com>
2024-06-25 16:53:50 -0300

    feat: add get data with query

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require(`mongoose`);
 
 const app = express();
-
+const bookRouter = express.Router();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books').get(async (req, res) => {
+  try {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    const books = await Book.find(query);
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon API!');
